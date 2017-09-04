b35610c0ba0907542b739a757075e6fb209a8904
Carlos Coello <carlos.coello05@outlook.com>
2017-09-04 13:24:24 -0500

    update server js index js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
-const path = require('path');
+const socketIO = require('socket.io');
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3200;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('createMessage', (message) => {
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
+});
+
+server.listen(port, () => {
   console.log(`Server running on port${port}`);
 })
